b45731139f644b132a43b0e7e8731c69b421256c
Akash <Akash@Skill-Lync.com>
2020-11-23 05:08:45 +0000

    First Commit

---
+++ src/test/LoginTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class LoginTest {
+
+	@Test
+	public void Testcase1() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+
+		String ActTitle = driver.getTitle();
+	
+		
+		Assert.assertEquals(ActTitle, "Facebook - Log In or Sign Up");
+		
+		
+		WebElement Uname = driver.findElement(By.id("email"));
+		WebElement Pwd = driver.findElement(By.id("pass"));
+		WebElement Button = driver.findElement(By.id("u_0_b"));
+		
+		Uname.sendKeys("Test123");
+		Pwd.sendKeys("1234");
+		Button.click();
+
+
+	}
+
+}
