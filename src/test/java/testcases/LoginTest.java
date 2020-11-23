0774547008e3f1491dcbcea01bbbcff73de33047
nikunjshah204gm <nikunjshah204gm@ip-172-31-10-141.ec2.internal>
2020-11-23 07:15:48 +0000

    Hello Worl Changes

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -16,7 +16,7 @@ public class LoginTest {
 	public void Testcase1() {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
